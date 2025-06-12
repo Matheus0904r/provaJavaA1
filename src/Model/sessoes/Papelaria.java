3741e71d1386b8eb0b3bb40ac606d48c7e9c9b87
matheusmahia <matheus.adsup@gmail.com>
2025-06-11 23:33:35 -0300

    criação seção Lazer  e Pet

---
+++ src/Model/sessoes/Papelaria.java
@@ -0,0 +1,13 @@
+package Model.sessoes;
+
+import Model.Produto;
+
+public abstract class Papelaria extends Produto {
+
+    private String cor;
+    private String marca;
+    private String especificacoes
+
+
+
+}
