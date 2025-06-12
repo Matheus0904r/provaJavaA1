8ceb04149492dc22df9608984bc9c62f1cbb41f2
SimpleNBO <simplenbo@gmail.com>
2025-06-12 00:14:08 -0300

    News End Models

---
+++ src/Model/objeto/Papel.java
@@ -0,0 +1,11 @@
+package Model.objeto;
+
+import Model.sessoes.Papelaria;
+
+public class Papel extends Papelaria {
+
+
+    public Papel(String nome, float preco, int quantidade, String cor, String marca, String especificacoes) {
+        super(nome, preco, quantidade, cor, marca, especificacoes);
+    }
+}
